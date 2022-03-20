bc97079126621ed87099c2482a0820a381bdfb88
Caleb Bolton <calbolt22@gmail.com>
2022-03-20 11:59:57 -0700

    Enhance benefits section and questionnaire link

--- docs/down/assets/js/scripts.js
+++ docs/down/assets/js/scripts.js
@@ -17,6 +17,11 @@ emailFormContainers.forEach((formContainer) => {
     closeButton.addEventListener("click", ()=>{closeModal(popup)});
     submitButton.addEventListener("click", ()=>{
         form.submit();
+        // Get email from current form
+        var email = formContainer.querySelector(":scope .text-box").value
+        // Prefill questionnaire with email
+        document.querySelectorAll(".thank-you a").forEach((a) => {a.href = "https://docs.google.com/forms/d/e/1FAIpQLSf_CSGif3vEyN7YZrHCj6nCHdGNc9G_fJWHhSwt8e7DMX9GTQ/viewform?usp=pp_url&entry.533643041=" + email;})
+        // Clear email values from all forms and swap forms for thank-you message
         document.querySelectorAll("form .text-box").forEach((textBox) => {textBox.value = ""});
         document.querySelectorAll(".email-form").forEach((emailForm) => {hideElement(emailForm)});
         document.querySelectorAll(".thank-you").forEach((thankYou) => {showElement(thankYou)});
